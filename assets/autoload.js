69c6ed367c342a0d58b3f0719253204c510bc021
Raymond Benc <r@phpfox.com>
2015-08-04 12:47:24 +0200

    Automated Dist: 1438685244

--- assets/autoload.js
+++ assets/autoload.js
@@ -1,6 +1,19 @@
 
 function pfRecaptchaCheck() {
 	$('#js_registration_submit').parents('form:first').trigger('submit');
+	// p(grecaptcha);
+};
+
+function pfRecaptchaLoad() {
+	var register = $('#js_registration_submit');
+	if (!register.length) {
+		return;
+	}
+
+	grecaptcha.render(document.getElementById('g-recaptcha-id'), {
+		sitekey : pf_recaptcha_key,
+		callback: pfRecaptchaCheck
+	});
 };
 
 $Ready(function() {
@@ -13,6 +26,9 @@ $Ready(function() {
 	if (register.length) {
 		var button = register.parent();
 
-		button.prepend('<div class="g-recaptcha" data-sitekey="' + pf_recaptcha_key + '" data-callback="pfRecaptchaCheck"></div>');
+		button.prepend('<div id="g-recaptcha-id"></div>');
+		if (typeof(grecaptcha) == 'object') {
+			pfRecaptchaLoad();
+		}
 	}
 });
